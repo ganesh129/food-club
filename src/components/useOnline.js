7665a4411ebf0791570d7435b89da418ba9fc5ff
ganesh129 <gksfromgaya@gmail.com>
2023-06-23 21:15:00 -0600

    useOnline custom Hook

---
+++ src/components/useOnline.js
@@ -0,0 +1,12 @@
+import { useState,useEffect } from "react";
+
+const useOnline=()=>{
+const [isOnline,setIsOnline]=useState(true);
+useEffect(()=>{
+   window.ononline=()=>setIsOnline(true);
+   window.onoffline=()=>setIsOnline(false)
+},[]);
+return isOnline;
+};
+
+export default useOnline;
